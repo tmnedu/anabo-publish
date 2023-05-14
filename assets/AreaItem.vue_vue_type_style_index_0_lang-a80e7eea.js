578157f18758f7b3e1bfe217237a2a2d3fb708f8
tmnedu <51187980+tmnedu@users.noreply.github.com>
2023-05-14 19:19:44 +0300

    dist

---
+++ assets/AreaItem.vue_vue_type_style_index_0_lang-a80e7eea.js
@@ -0,0 +1,1 @@
+import{d as a,o as n,a as r,B as t,h as o}from"./index-09f4c865.js";const c=["innerHTML"],_=a({__name:"AreaItem",props:{area:null},setup(e){return(l,s)=>e.area?(n(),r("div",{key:0,class:"area-item",style:t(`background-color: ${e.area.color}; color: #aaa`),innerHTML:e.area.name},null,12,c)):o("",!0)}});export{_};
